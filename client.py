67840152304165d00cefb191f9df12c3a6ad696e
Watkins7 <71716327+Watkins7@users.noreply.github.com>
2022-03-13 15:14:48 -0400

    Removed files

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 from ftplib import FTP
-import io
-import os
+import io, sys
+import os, signal
 import shutil
 from pathlib import Path
 
@@ -15,17 +15,14 @@ is_file = ["F", "FILE", "f", "file", ]
 is_directory = ["D", "DIRECTORY", "d", "directory"]
 currentDirectory = ""
 
+
 # Prompts user for a server IP
 # If successful, prompts user for credentials
 # Returns a FTP object on success
 def connect_to_server():
 
-    global clientConnection     # Global server IPS
-    new_ftp = FTP()             # return object
-
-
-    os.mkdir("test")
-    os.chmod("test", 0o222)
+    global clientConnection  # Global server IPS
+    new_ftp = FTP()  # return object
 
     # find a server connection
     while 1:
@@ -56,7 +53,7 @@ def connect_to_server():
         try:
             new_ftp.login(username, password)
             print(" << Login Success!")
-            new_ftp.set_pasv(True)                      # Set to passive mode if time out
+            new_ftp.set_pasv(True)  # Set to passive mode if time out
             break
 
         except Exception as e:
@@ -68,46 +65,35 @@ def connect_to_server():
         if response in listOfYes:
             return False
 
-    list_of_known_servers.append(serverIP)          # Append server information
-    return new_ftp                                  # return FTP object
+    list_of_known_servers.append(serverIP)  # Append server information
+    return new_ftp  # return FTP object
 
 
 # Makes a blank file or directory in SEDFS
-def create_blank_file_or_directory():
+def create_blank_file():
 
-    # loop until user says 'FILE' or 'DIRECTORY'
-    while True:
-        print(" >> File (F) or Directory (D)\n >> ", end='')
-        response = input()
+    print("Create file name\n >> ", end='')
+    client_file = input()
+    command = 'STOR ' + client_file
 
-        if response in is_file or response in is_directory:
-            break
+    try:
+        response = ftp.storbinary(command, io.BytesIO(b''))
+        print(" << ", response)
 
-    # create BLANK FILE
-    if response in is_file:
-        print("Create file name\n >> ", end='')
-        client_file = input()
-        command = 'STOR ' + client_file
+    except Exception as e:
+        print(e)
 
-        try:
-            response = ftp.storbinary(command, io.BytesIO(b''))
-            print(" << ", response)
+def create_blank_directory():
 
-        except Exception as e:
-            print(" << ERROR:", e)
+    print("Create directory name\n >> ", end='')
+    client_directory = input()
 
+    try:
+        response = ftp.mkd(client_directory)
+        print(" << ", response)
 
-    # create BLANK DIRECTORY
-    else:
-        print("Create directory name\n >> ", end='')
-        client_directory = input()
-
-        try:
-            response = ftp.mkd(client_directory)
-            print(" << ", response)
-
-        except Exception as e:
-            print(" << ERROR:", e)
+    except Exception as e:
+        print(" << ERROR:", e)
 
 
 # Open Text Editor
@@ -136,8 +122,8 @@ def open_program():
         print(e)
 
 
-# Delete 'file' or 'directory'
-def delete(ftp):
+# Delete 'file'
+def file_delete(ftp):
 
     name = input("Enter name to delete\n >> ")
 
@@ -152,11 +138,9 @@ def delete(ftp):
     if ans in listOfNo:
         try:
             ftp.delete(name)
-            return
 
         except Exception as e:
             print(e)
-            return
 
     else:
         print("Enter existing path\n >> ", end='')
@@ -164,11 +148,39 @@ def delete(ftp):
 
         try:
             ftp.delete(new_path + name)
-            return
 
         except Exception as e:
             print(e)
-            return
+
+# Delete 'Directory'
+def directory_delete(ftp):
+
+    name = input("Enter name to delete\n >> ")
+
+    # Ask if user wants new path
+    while True:
+        print("Enter new path?\n >> ", end='')
+        ans = input().lower()
+
+        if ans in listOfYes or ans in listOfNo:
+            break
+
+    if ans in listOfNo:
+        try:
+            ftp.rmd(name)
+
+        except Exception as e:
+            print(e)
+
+    else:
+        print("Enter existing path\n >> ", end='')
+        new_path = input()
+
+        try:
+            ftp.rmd(new_path + name)
+
+        except Exception as e:
+            print(e)
 
 
 # navigate to new folder
@@ -178,21 +190,33 @@ def navigate(ftp):
 
     try:
         ftp.cwd(new_path)
-        currentDirectory = new_path
+        # currentDirectory = new_path
 
-    except Execption as e:
+    except Exception as e:
         print(e)
+
+
+def rename(ftp):
+
+    old_name = input("Enter old name\n >> ")
+    new_name = input("Enter new name\n >> ")
+
+    try:
+        resp = ftp.rename(old_name, new_name)
+    except Exception as E:
+        print(E)
+
+    print(resp)
 
 
 # list all current files and directories
 def ftp_list(ftp):
-    try:
-        # ftp.retrlines('LIST')
-        # ftp.retrlines('LIST *README*')
-        all_objects = ftp.nlst()
 
-        for obj in all_objects:
-            print(obj)
+    try:
+
+        print("\n\n-------Begin of List------\n")
+        ftp.retrlines('LIST')
+        print("\n-------End of List------\n\n")
 
     except Exception as E:
         print("Error: ", E)
@@ -202,64 +226,96 @@ def ftp_list(ftp):
 def change_permissions(ftp):
     filename = input("Input filename\n >> ")
     permissions = input("Input new permissions\n >> ").strip()
-    ftp.sendcmd("SITE " + permissions + " " + filename)
+    try:
+        ftp.sendcmd("SITE CHMOD " + permissions + " " + filename)
+    except Exception as E:
+        print(E)
+
+
+"""
+def change_owner(ftp):
+    filename = input("Input filename\n >> ")
+    owner = input("Input new owner\n >> ").strip()
+    try:
+        ftp.sendcmd("SITE CHOWN " + owner + " " + filename)
+    except Exception as E:
+        print(E)
+"""
 
 
 # Display Help Menu
 def help(ftp):
     print("============================\n",
-          "\nCurrent Path: " + ftp.pwd() + "\n\n",
-          "\t'q' == Quit SEDFS\n",
-          "\t'r' == Read SEDFS file\n",
+          # "\nCurrent Path: " + currentDirectory + "\n\n",
+          "\t'cf' == Create new SEDFS file\n",
+          "\t'cd' == Create new SEDFS directory\n",
           "\t'w' == Write to SEDFS\n",
-          "\t'p' == Change permissions\n",
-          "\t'c' == Create new SEDFS file/directory\n",
+          "\t'r' == Read SEDFS file\n",
+          "\t'l' == List directory contents contents\n",
           "\t'n' == Navigate to new directory\n",
           "\t'b' == Move back 1 directory\n",
-          "\t'l' == List directory contents contents\n",
-          "\t'd' == Delete file/directory\n",
+          "\t'u' == Change Name\n",
+          "\t'p' == Change permissions\n",
+          "\t'df' == Delete file\n",
+          "\t'dd' == Delete directory\n",
+
+          #"\t'k' == Change Owner\n",
+
           "\t's' == Display Server Information\n",
           "\t'o' == Open Text Editor\n",
+          "\t'q' == Quit SEDFS\n",
           "\t'h' == Help\n")
 
 
-# write to SEDFS
+# WRITE to SEDFS
 def write(ftp):
-
-    local_name = input("Local file to upload\n >> ")
+    local_name = input("Enter Local file path to upload\n >> ")
     try:
+        print("\n-------File uploading started------")
         file = open(local_name, 'rb')
     except Exception as E:
         print(E)
         return
 
     try:
-        ftp.storbinary('STOR ', local_name, file)  # send the file
+        ftp.storbinary('STOR ' + local_name, file)  # send the file
         file.close()
+        print("-------File has uploaded successfully------\n\n")
     except Exception as E:
         print(E)
 
-# read from sedfs
-def read(ftp):
 
-    sedfs_name = input("SEDFS file to download\n >> ")
+# READ from SEDFS
+def read(ftp):
+    sedfs_name = input("Enter SEDFS file path to download\n >> ")
     try:
-        ftp.retrbinary("RETR ",sedfs_name, print)
+        print("\n\n-------Begin------\n")
+        #ftp.retrbinary("RETR " + sedfs_name, print)
+        respMessage = ftp.retrlines("RETR " + sedfs_name, print)
+        print("\n-------EOF------\n\n")
     except Exception as E:
         print(E)
         return
 
 
+# ../
 def go_back(ftp):
-
     try:
         ftp.cwd("../")
+        # currentDirectory = ftp.pwd()
     except Exception as E:
         print(E)
 
+#####
+#insert EXECPTION Class
 
-class Execption:
-    pass
+# Capture 'Control + C'
+def signal_handler(sig, frame):
+    print("Exiting client.py")
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
 
 if __name__ == '__main__':
 
@@ -268,33 +324,34 @@ if __name__ == '__main__':
     if ftp:
 
         help(ftp)
-        currentDirectory = ftp.pwd()
 
         while 1:
-            print("%s >> " % currentDirectory, end='')
+
+            print("Enter a command (or type 'h' to see the menu) >> ", end='')
             clientRequest = input().lower()
 
-            # Create
-            if clientRequest == "create" or clientRequest == "c":
-                try:
-                    create_blank_file_or_directory()
-                    print(" << SUCCESS")
+            # Create blank file
+            if clientRequest == "create" or clientRequest == "cf" or clientRequest == "touch":
+                create_blank_file()
 
-                except Exception as e:
-                    print(" << ERROR")
+            # Create directory
+            elif clientRequest == "create directory" or clientRequest == "cd" or clientRequest == "mkdir":
+                create_blank_directory()
 
             # Write
             elif clientRequest == "write" or clientRequest == "w":
-                print("Not Tested")
                 write(ftp)
 
             # read
             elif clientRequest == "read" or clientRequest == "r":
-                print("Not Tested")
                 read(ftp)
 
+            # read
+            elif clientRequest == "rename" or clientRequest == "u":
+                rename(ftp)
+
             # change permissions
-            elif clientRequest == "permissions" or clientRequest == "p":
+            elif clientRequest == "permissions" or clientRequest == "p" or clientRequest == "chmod":
                 print("Not tested")
                 change_permissions(ftp)
 
@@ -302,22 +359,29 @@ if __name__ == '__main__':
             elif clientRequest == "n" or clientRequest == "navigate":
                 navigate(ftp)
 
+            # Navigate
+            #elif clientRequest == "k" or clientRequest == "chown":
+            #    change_owner(ftp)
+
             # Back 1 Directory
             elif clientRequest == "b" or clientRequest == "back":
                 print("Not tested")
                 go_back(ftp)
 
-            # Delete
-            elif clientRequest == "d" or clientRequest == "delete":
-                delete(ftp)
+            # Delete file
+            elif clientRequest == "df" or clientRequest == "delete file" or clientRequest == "rf":
+                file_delete(ftp)
+
+            elif clientRequest == "dd" or clientRequest == "delete directory" or clientRequest == "rmdir":
+                directory_delete(ftp)
 
             # List
-            elif clientRequest == "l" or clientRequest == "list":
+            elif clientRequest == "l" or clientRequest == "list" or clientRequest == "ls":
                 ftp_list(ftp)
 
             # Open program with file
-            elif clientRequest == "o" or clientRequest == "open" or clientRequest == "text editor" or\
-                clientRequest == "open editor" or clientRequest == "open text editor" or\
+            elif clientRequest == "o" or clientRequest == "open" or clientRequest == "text editor" or \
+                    clientRequest == "open editor" or clientRequest == "open text editor" or \
                     clientRequest == "open text":
 
                 open_program()
--- server.py
+++ server.py
@@ -1,31 +1,50 @@
-import os
 from fileinput import fileno
 
-from pyftpdlib.authorizers import UnixAuthorizer
+# for file server
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
+from pyftpdlib.authorizers import UnixAuthorizer
+from pyftpdlib.filesystems import AbstractedFS
 from pyftpdlib.filesystems import UnixFilesystem
 
-import socket
+# ip address
+from subprocess import check_output
+import netifaces as ni
+
+# For file sync
+from ftplib import FTP
+
+# General Libs need for OS
+import os, sys
+from os import path
+from os.path import exists
 
 # Used to display logging to stdout
 import logging.config
-import sys
 
+# Date and time
 from datetime import datetime
+
+#######################################
+# Exit if user is not sudo
+#######################################
+if os.getuid() != 0:
+    print("Error: Server needs to be run as a sudo user\nGoodbye.")
+    sys.exit(0)
 
 #######################################
 # Global Logger
 #######################################
 # Set logging configuration
 
-logging.basicConfig(filename='fileServer.log', level=logging.DEBUG, filemode='w',
+"""
+logging.basicConfig(filename='SEDFS_EVENT_LOG.log', level=logging.DEBUG, filemode='w',
                     format='%(asctime)s\tLogger: %(name)s\tLevel: %(levelname)s\tEvent: %(message)s',
                     datefmt='%Y:%m:%d %H:%M:%S')
-
-# logging.basicConfig(filename='fileServer.log', level=logging.DEBUG, filemode='w',
-# format='%(asctime)s\tLogger: %(name)s\tLevel: %(levelname)s\tEvent: %(message)s',
-# datefmt='%Y:%m:%d %H:%M:%S')
+"""
+logging.basicConfig(filename='SEDFS_EVENT_LOG.log', level=logging.DEBUG, filemode='w',
+                    format='%(asctime)s\tLogger: %(name)s\tEvent: %(message)s',
+                    datefmt='%d/%m: %H/%M/%S')
 
 # Create Logger
 serverLog = logging.getLogger("SEDFS Server")
@@ -36,6 +55,7 @@ serverLog.addHandler(loggingHandler)
 serverLog.setLevel(logging.DEBUG)
 
 known_servers = []
+known_users = []
 listOfNo = ["no", "n", "NO", "N"]
 
 
@@ -55,13 +75,35 @@ class SEDFS_handler(FTPHandler):
     def __init__(self, conn, server, ioloop):
         self.handler_user = None
         self.handler_pass = None
+        self.current_path = "/"
+        # self.virtual_filesystem = AbstractedFS("/SEDFS", self)
 
         # Log handler event
-        print(datetime.now().strftime("DATE: %Y:%m:%d\tTIME: %H:%M:%S\tEVENT: "), end="")
-        serverLog.info("[+] SEDFS Handle Started")
+        ip = str(conn.getpeername())
+
+        print(datetime.now().strftime("DATE: %Y:%m:%d /// TIME: %H:%M:%S /// EVENT: "), end="")
+        log_event = "[+] SEDFS Handle Started at: " + ip
+        serverLog.info(log_event)
 
         # parent handler init
         FTPHandler.__init__(self, conn, server)
+
+        self.proto_cmds.update(
+            {'SITE SIDEMKD': dict(perm='m', auth=True, arg=True, path=True,
+                                  help='Side channel for make dir on other servers'),
+             'SITE SIDEDELE': dict(perm='d', auth=True, arg=True,
+                                   help='Side channel for delete file on other server'),
+             'SITE SIDERNFR': dict(perm='f', auth=True, arg=True,
+                                   help='One/two side channel for rename'),
+             'SITE SIDERNTO': dict(perm=None, auth=True, arg=True,
+                                   help='two/two side channel for rename'),
+             'SITE SIDECHMOD': dict(perm='M', auth=True, arg=True,
+                                    help='Side channel for permissions'),
+             'SITE SIDESTOR': dict(perm='w', auth=True, arg=True,
+                                   help='Side channel for storing files'),
+             'SITE SIDERMD': dict(perm='d', auth=True, arg=True,
+                                  help='Side channel for remove directory')}
+        )
 
         return
 
@@ -72,7 +114,199 @@ class SEDFS_handler(FTPHandler):
         print(self.handler_user)
         print(self.handler_pass)
 
-        FTPHandler.on_login(username)
+        try:
+            print(self.fs.cwd)
+        except Exception as E:
+            print(E)
+
+        super().on_login(username)
+
+    def on_file_received(self, file):
+
+        try:
+            print(self.fs.cwd)
+        except Exception as E:
+            print(E)
+
+        super().on_file_received(file)
+
+    def ftp_CWD(self, path):
+
+        super().ftp_CWD(path)
+
+        try:
+            self.current_path = self.fs.cwd
+        except Exception as E:
+            print(E)
+
+    ####################################################################################
+    # Make Directory on Server and ALL other server
+    def ftp_MKD(self, path):
+
+        new_path = self.fs.fs2ftp(path)
+        current_path = self.fs.cwd
+        # current_path = self.fs.fs2ftp(current_path)
+
+        print("Testing multi MKD", new_path)
+        print("Current path is ", current_path)
+
+        # Make directory in all other servers
+        for i in known_servers:
+            new_FTP = FTP()
+            new_FTP.connect(i, 50000)
+            new_FTP.login(self.handler_user, self.handler_pass)
+            try:
+                new_FTP.cwd(self.fs.cwd)
+            except Exception as E:
+                print(E)
+            try:
+                new_FTP.sendcmd("SITE SIDEMKD " + new_path)
+            except Exception as E:
+                print(E)
+            new_FTP.close()
+
+        super().ftp_MKD(path)
+
+    # Sidechannel on making directory only
+    def ftp_SITE_SIDEMKD(self, path):
+        actual_path = self.fs.fs2ftp(path)
+        actual_path = self.fs.ftp2fs(actual_path)
+        print(actual_path)
+
+        super().ftp_MKD(actual_path)
+
+    ####################################################################################
+    # Renaming on Server and ALL other Server
+    def ftp_RNFR(self, path):
+
+        new_path = self.fs.fs2ftp(path)
+
+        print("Testing multi RNFR", new_path)
+
+        # Make directory in all other servers
+        for i in known_servers:
+            new_FTP = FTP()
+            new_FTP.connect(i, 50000)
+            new_FTP.login(self.handler_user, self.handler_pass)
+            new_FTP.cwd(self.current_path)
+            new_FTP.sendcmd("SITE SIDERNFR " + new_path)
+            new_FTP.close()
+
+        super().ftp_RNFR(path)
+
+    def ftp_RNTO(self, path):
+
+        new_path = self.fs.fs2ftp(path)
+
+        print("Testing multi RNTO", path)
+
+        # Make directory in all other servers
+        for i in known_servers:
+            new_FTP = FTP()
+            new_FTP.connect(i, 50000)
+            new_FTP.login(self.handler_user, self.handler_pass)
+            new_FTP.cwd(self.current_path)
+            new_FTP.sendcmd("SITE SIDERNTO " + new_path)
+            new_FTP.close()
+
+        super().ftp_RNTO(path)
+
+    # Sidechannel on renaming only
+    def ftp_SITE_SIDERNFR(self, path):
+        super().ftp_RNFR(path)
+
+    def ftp_SITE_SIDERNTO(self, path):
+        super().ftp_RNTO(path)
+
+    ####################################################################################
+    # Make File on server
+    def ftp_STOR(self, file, mode='w'):
+
+        print("Testing multi STOR")
+
+        # Make directory in all other servers
+        for i in known_servers:
+            new_FTP = FTP()
+            new_FTP.connect(i, 50000)
+            new_FTP.login(self.handler_user, self.handler_pass)
+            new_FTP.cwd(self.current_path)
+            new_FTP.sendcmd("SITE SIDESTOR " + file)
+            new_FTP.close()
+
+        super().ftp_STOR(file, mode)
+
+    # Sidechannel making file on other servers
+    def ftp_SIDESTOR(self, file, mode='w'):
+        super().ftp_STOR(file, mode)
+
+    ####################################################################################
+    # Delete File on server
+    def ftp_DELE(self, path):
+
+        new_path = self.fs.fs2ftp(path)
+
+        print("Testing multi DELE", new_path)
+
+        # Make directory in all other servers
+        for i in known_servers:
+            new_FTP = FTP()
+            new_FTP.connect(i, 50000)
+            new_FTP.login(self.handler_user, self.handler_pass)
+            new_FTP.cwd(self.current_path)
+            new_FTP.sendcmd("SITE SIDEDELE " + new_path)
+            new_FTP.close()
+
+        super().ftp_DELE(path)
+
+    # Sidechannel deleting file on other servers
+    def ftp_SIDEDELE(self, path):
+        super().ftp_DELE(path)
+
+    ####################################################################################
+    # Delete directory on server
+    def ftp_RMD(self, path):
+
+        new_path = self.fs.fs2ftp(path)
+
+        print("Testing multi RNFR", new_path)
+
+        # Make directory in all other servers
+        for i in known_servers:
+            new_FTP = FTP()
+            new_FTP.connect(i, 50000)
+            new_FTP.login(self.handler_user, self.handler_pass)
+            new_FTP.cwd(self.current_path)
+            new_FTP.sendcmd("SITE SIDERMD " + new_path)
+            new_FTP.close()
+
+        super().ftp_RMD(path)
+
+    # Sidechannel delete other files on server
+    def ftp_SIDERMD(self, path):
+        super().ftp_RMD(path)
+
+    ####################################################################################
+    # Change permissions on server
+    def ftp_SITE_CHMOD(self, path, mode):
+
+        new_path = self.fs.fs2ftp(path)
+
+        print("Testing multi CHMOD", new_path, mode)
+
+        # Make directory in all other servers
+        for i in known_servers:
+            new_FTP = FTP()
+            new_FTP.connect(i, 50000)
+            new_FTP.login(self.handler_user, self.handler_pass)
+            new_FTP.cwd(self.current_path)
+            new_FTP.sendcmd("SITE SIDECHMOD " + mode + " " + new_path)
+            new_FTP.close()
+
+        super().ftp_SITE_CHMOD(path, mode)
+
+    # Sidechannel changing permissions on other servers
+    def ftp_SITE_SIDECHMOD(self, path, mode):
+        super().ftp_SITE_CHMOD(path, mode)
 
 
 class SEDFS_authorizer(UnixAuthorizer):
@@ -93,8 +327,8 @@ class SEDFS_authorizer(UnixAuthorizer):
     def validate_authentication(self, username, password, handler):
         self.user_is = username
         self.pass_is = password
-        print(self.user_is)
-        print(self.pass_is)
+        # print(self.user_is)
+        # print(self.pass_is)
 
         UnixAuthorizer.validate_authentication(self, username, password, handler)
 
@@ -105,10 +339,75 @@ class SEDFS_authorizer(UnixAuthorizer):
         return self.pass_is
 
 
+def multiServerLoad():
+    global known_servers
+
+    if exists("configuration files/knownServers.txt"):
+
+        print("Multi-Server Configuration File Found")
+
+        ans = input(" Load File (y/yes) >> ").lower()
+        if ans == "yes" or ans == "y":
+            my_file = open("configuration files/knownServers.txt", "r")
+            content = my_file.readlines()
+            my_file.close()
+            for i in range(len(content)):
+                content[i] = content[i].strip()
+                known_servers.append(content[i])
+
+        else:
+            print("Multi-Server Config not loaded")
+
+    else:
+        print("Warning... No multi-server configuration file found")
+
+
+def multiUserLoad():
+    global known_users
+
+    if exists("configuration files/userConfig.txt"):
+
+        print("User Configuration File Found")
+
+        ans = input(" Load File (y/yes) >> ").lower()
+        if ans == "yes" or ans == "y":
+            my_file = open("configuration files/userConfig.txt", "r")
+            content = my_file.readlines()
+            my_file.close()
+            for i in range(len(content)):
+                content[i] = content[i].strip()
+                known_users.append(content[i])
+
+        else:
+            print("WARNING... Multi-user Config not loaded!!!")
+            print("WARNING... No users are authorized to use server")
+
+    else:
+        print("WARNING... No multi-user configuration file found!!!")
+        print("WARNING... No users are authorized to use server")
+
+
 def SEDFS_setup():
-    # Get local address information
-    server_IP = socket.gethostbyname(socket.gethostname())
+    # List of IPS on system
+    ips = check_output(['hostname', '--all-ip-addresses']).strip()
+
+    print("\n-----------Welcome Admin----------\n")
+    print(" >> A list of all possible IPS on your system\n", ips)
+    print("\n")
+
+    try:
+        ni.ifaddresses('eth0')
+        server_IP = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
+
+    except Exception as E:
+        print(E)
+        print("By default SEDFS uses eth0, please edit code to select default interface")
+        print("Goodbye")
+        return
+
     address = (server_IP, 50000)
+    print(" >> Default Server IP: ", server_IP)
+    print(" >> Default Server Port: ", 50000)
 
     # Instantiate a dummy authorizer for managing 'virtual' users
     authorizer = SEDFS_authorizer()
@@ -117,15 +416,41 @@ def SEDFS_setup():
     if not os.path.exists("SEDFS"):
         os.mkdir("SEDFS")
 
+    # Make sure that the directory is public
+    os.chmod("SEDFS", 0o777)
+
     # LOAD username, password, home directory, permissions
-    authorizer.override_user(username='sara', homedir='./SEDFS')
-    # load_users(authorizer)
+    multiUserLoad()
+    for i in range(len(known_users)):
+        try:
+            home = os.path.abspath("SEDFS")
+            authorizer.override_user(known_users[i], homedir=home)
+        except Exception as E:
+            print(E)
+
+    # LOAD multi-server config
+    multiServerLoad()
 
     # Instantiate FTP handler class
     handler = SEDFS_handler
-    handler.abstract_fs = UnixFilesystem
+    # handler.abstract_fs = UnixFilesystem("/SEDFS", handler)
+    handler.abstracted_fs = AbstractedFS
+
+    # testing code
+    """
+    try:
+    #    variable = AbstractedFS("/SEDFS", handler)
+        print(handler.abstract_fs.cwd)
+
+    except Exception as E:
+        print(E)
+    """
+
     handler.authorizer = authorizer
+    handler.timeout = None
     handler.banner = " << Welcome to SEDFS (Simple Encrypted Distributed File System)"
+    #handler.abstracted_fs = AbstractedFS("/SEDFS",handler)
+    #handler.abstracted_fs = UnixFilesystem
 
     # Instantiate FTP server class and listen on ??????:50000
     server = SEDFS_server(address, handler)
@@ -134,9 +459,18 @@ def SEDFS_setup():
     server.max_cons = 100000
     server.max_cons_per_ip = 5
 
+    print(" >> Server Setup complete...")
+    print(" >> SEDFS Server Online")
+
     # start ftp server
     server.serve_forever()
 
 
 if __name__ == '__main__':
+
+    if path.exists("/configuration files/knownServers.txt"):
+        my_file = open("/configuration files/knownServers.txt", "r")
+        content = my_file.read()
+        print(content)
+
     SEDFS_setup()
